fbc3782b96b2e7c72e204022fcdda169f036848d
Ashirvad Satapathy <ashirvad.satapathy01@gmail.com>
2024-02-07 22:04:02 +0530

    feat: credential auth darft 1

--- src/tests/getSessionTest.ts
+++
@@ -1,8 +0,0 @@
-import { AuthService } from '../modules/auth/auth.services';
-
-async function Yotest() {
-  const session = await AuthService.getSessionFromRefreshToken('ashirvad@gmail.com');
-  console.log('session :', session);
-}
-
-Yotest();
